4ac8fb930ff39cc3bf017af7205ee0612eea72a4
Linda Albrektsson <linda.albrektsson@gmail.com>
2026-09-10 12:59:24 +0200

    Bugg 5: hämta rätt inlägg baserat på id

---
+++ src/components/Post.bugg5.test.tsx
@@ -0,0 +1,27 @@
+import { render } from "@testing-library/react";
+import { afterEach, beforeEach, vi } from "vitest";
+import Post from "./Post";
+
+//Bugg 5 — Fel inlägg visas
+
+describe("Post", () => {
+  beforeEach(() => {
+    globalThis.fetch = vi.fn().mockResolvedValue({
+      json: async () => ({}),
+    });
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  it("ska göra fetchanrop baserat på id", () => {
+    // Act
+    render(<Post id={2} />);
+
+    // Assert
+    expect(globalThis.fetch).toHaveBeenCalledWith(
+      "https://jsonplaceholder.typicode.com/posts/2",
+    );
+  });
+});
